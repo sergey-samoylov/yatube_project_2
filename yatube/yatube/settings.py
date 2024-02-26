2dd968e06189e9382a450f1d86e552879e69ad0e
Sergey Samoylov <110048435+sergey-samoylov@users.noreply.github.com>
2024-02-26 22:39:06 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -13,7 +13,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-wp-h241z#+!*#g=rw9rc7-!2n^ml-6l#-zbl5redh8va3e4kb)'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
